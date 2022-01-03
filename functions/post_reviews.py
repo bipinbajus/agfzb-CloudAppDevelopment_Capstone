92133d8ade46b4c54bb46ab29c3cf2af9804b1a7
bipinbajus <bipinbaj.us@gmail.com>
2022-01-03 14:09:24 +1100

    Create post_reviews.py

---
+++ functions/post_reviews.py
@@ -0,0 +1,39 @@
+#
+#
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
+#
+from cloudant.client import Cloudant
+from cloudant.error import CloudantException
+import requests
+
+
+def main(dict):
+    
+    try:
+        client = Cloudant.iam(
+            account_name=dict["COUCH_USERNAME"],
+            api_key=dict["IAM_API_KEY"],
+            connect=True,
+        )
+        
+        my_database = client['reviews']
+        
+        # Create a document using the Database API
+        my_document = my_database.create_document(dict['review'])
+        
+        # Check that the document exists in the database
+        if my_document.exists():
+            print('SUCCESS!!')
+            return {'success': 'Review Added!'}
+        
+    except CloudantException as ce:
+        print("unable to connect")
+        return {"error": ce}
+    except (requests.exceptions.RequestException, ConnectionResetError) as err:
+        print("connection error")
+        return {"error": err}
