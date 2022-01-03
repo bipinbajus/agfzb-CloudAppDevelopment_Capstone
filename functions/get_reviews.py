839577d1358572914733f81138129a38be392f98
bipinbajus <bipinbaj.us@gmail.com>
2022-01-03 14:07:05 +1100

    get reviews

---
+++ functions/get_reviews.py
@@ -0,0 +1,40 @@
+#
+#
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
+#
+from cloudant.client import Cloudant
+from cloudant.error import CloudantException
+import requests
+
+
+def main(dict):
+    
+    try:
+        client = Cloudant.iam(
+            account_name=dict["COUCH_USERNAME"],
+            api_key=dict["IAM_API_KEY"],
+            connect=True,
+        )
+        
+        my_database = client['reviews']
+        
+        reviewlist = []
+        
+        for document in my_database:
+            if document['id'] == int(dict["dealerId"]):
+                reviewlist.append(document)
+        
+        return {"data": reviewlist }
+        
+        #print("Databases: {0}".format(client.all_dbs()))
+    except CloudantException as ce:
+        print("unable to connect")
+        return {"error": ce}
+    except (requests.exceptions.RequestException, ConnectionResetError) as err:
+        print("connection error")
+        return {"error": err}
